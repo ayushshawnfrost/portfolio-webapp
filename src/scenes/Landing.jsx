3d0847376a7ab31f5eabe7d9a54876f601751287
STSC <STSC@LENOVO-131229.ad.csueastbay.edu>
2023-01-21 15:55:49 -0800

    mobile site profile fix

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -28,7 +28,7 @@ const Landing = ({ setSelectedPage }) => {
           <img
             alt="profile"
             className="z-10 w-full max-w-[400px] md:max-w-[600px]"
-            src="assets/pot.png"
+            src={profile}
           />
         )}
       </div>
